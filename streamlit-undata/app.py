4c8394a2afc25d4f4d467c42d18743fd9b30fffe
Muhamad Faridi Dahlan <152169038+faridi-dahlan@users.noreply.github.com>
2024-02-26 18:10:31 +0700

    Update app.py

--- streamlit-undata/app.py
+++ streamlit-undata/app.py
@@ -1,6 +1,6 @@
 #import libraries
 import streamlit as st
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu import option_menu
 import pandas as pd
 import altair as alt
 import numpy as np
